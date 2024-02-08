69ca745b4f19fa72ed96098271a84752a91618f8
Kavin Muralikrishnan <921563@lcps.org>
2024-02-08 18:24:17 -0500

    Commented out turnToTag method because a command exists

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -95,24 +95,24 @@ Field2d m_field = new Field2d();
    * passing it to a
    * {@link JoystickButton}.
    */
-  public void turnToTag(){
-    final double ANGULAR_P = 0.1;
-    final double ANGULAR_D = 0.0;
-    PIDController turnController = new PIDController(ANGULAR_P, 0, ANGULAR_D);
-    if(!Vision.hasResults()){
-      return;
-    }
-    else{
-      double angle = Vision.getTargetPitch(false);
-      //Write code to turn towards the tag(need to figure out a way to turn while maintaining speed. Oh wait I got it.)
-      m_robotDrive.drive(
-              -MathUtil.applyDeadband(m_driverController.getLeftY(), OIConstants.kDriveDeadband),
-              -MathUtil.applyDeadband(m_driverController.getLeftX(), OIConstants.kDriveDeadband),
-              -MathUtil.applyDeadband(turnController.calculate(Vision.getTargetYaw(false)), OIConstants.kDriveDeadband),
-                true, true);
-    }
-    m_robotDrive.drive(0,0,0,true,true);
-  }
+  // public void turnToTag(){
+  //   final double ANGULAR_P = 0.1;
+  //   final double ANGULAR_D = 0.0;
+  //   PIDController turnController = new PIDController(ANGULAR_P, 0, ANGULAR_D);
+  //   if(!Vision.hasResults()){
+  //     return;
+  //   }
+  //   else{
+  //     double angle = Vision.getTargetPitch(false);
+  //     //Write code to turn towards the tag(need to figure out a way to turn while maintaining speed. Oh wait I got it.)
+  //     m_robotDrive.drive(
+  //             -MathUtil.applyDeadband(m_driverController.getLeftY(), OIConstants.kDriveDeadband),
+  //             -MathUtil.applyDeadband(m_driverController.getLeftX(), OIConstants.kDriveDeadband),
+  //             -MathUtil.applyDeadband(turnController.calculate(Vision.getTargetYaw(false)), OIConstants.kDriveDeadband),
+  //               true, true);
+  //   }
+  //   m_robotDrive.drive(0,0,0,true,true);
+  // }
   public Command turnToTagCommand(){
     final double ANGULAR_P = 0.1;
     final double ANGULAR_D = 0.0;
