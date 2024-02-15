6969d2d0133ab6e077e83c11e0d27b25be314858
Kavin Muralikrishnan <921563@lcps.org>
2024-02-15 18:30:53 -0500

    I think something was fixed

--- src/main/java/frc/utils/ShootingUtils.java
+++ src/main/java/frc/utils/ShootingUtils.java
@@ -1,12 +1,12 @@
 package frc.utils;
 
-import edu.wpi.first.math.util.Units;
+
 import frc.robot.Constants;
 
 public class ShootingUtils {
 
     private static final double OFFSET_ANGLE_DEGREES = 40; // degrees
-    private static final double SPEAKER_TAG_HEIGHT = 0.2667; // meters
+    private static final double SPEAKER_TAG_HEIGHT = Constants.aprilTagUniqueHeights[2]+Constants.plasticTagLength/2; // meters
     private static final double DISTANCE_TAG_TO_SPEAKER = Constants.SPEAKER_HEIGHT_METERS - SPEAKER_TAG_HEIGHT; // meters
 
     private static final double DISTANCE_CAMERA_TO_TAG_Y = SPEAKER_TAG_HEIGHT - Constants.CAMERA_HEIGHT_METERS;
