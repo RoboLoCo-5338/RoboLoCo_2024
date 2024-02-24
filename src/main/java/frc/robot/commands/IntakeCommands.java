318e9384f372770264879acbc99b58dcf62112dd
RoboLoCo, Team 5338 <team5338@gmail.com>
2024-02-23 20:10:02 -0500

    added arm commands

--- src/main/java/frc/robot/commands/IntakeCommands.java
+++ src/main/java/frc/robot/commands/IntakeCommands.java
@@ -1,5 +1,30 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.RobotContainer;
+
 public class IntakeCommands {
     
+    public static Command arm() {
+      return new InstantCommand(
+        () -> RobotContainer.m_shooter.shooterReverse(),
+        RobotContainer.m_shooter
+      );
+    }
+
+    public static Command shooterForward() {
+      return new InstantCommand(
+        () -> RobotContainer.m_shooter.shooterForward(),
+        RobotContainer.m_shooter
+      );
+
+    }
+
+    public static Command shooterStop() {
+      return new InstantCommand(
+        () -> RobotContainer.m_shooter.shooterStop(),
+        RobotContainer.m_shooter
+      );
+    }
 }
