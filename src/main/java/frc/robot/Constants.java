5807577d0a9e3e54bbdc348fc7772c3d8ee63323
RoboLoCo, Team 5338 <team5338@gmail.com>
2024-03-02 16:24:07 -0500

    changed deadband

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -229,7 +229,7 @@ public final class Constants {
   public static final class OIConstants {
     public static final int kDriverControllerPort = 0;
     public static final int kOperatorControllerPort = 1;
-    public static final double kDriveDeadband = 0.10;
+    public static final double kDriveDeadband = 0.05;
     public static final double kArmDeadband = 0.10;
   }
 
